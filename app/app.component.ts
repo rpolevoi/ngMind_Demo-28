e1edd54aa6fd43f831e9a3c026047527ab3b224c
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-25 16:34:19 -0700

    Create app.component.ts

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MemberService } from './member.service';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'root',
+  template: `<h1>Parent View Here</h1>
+    <p [style.color]="membServ.selected ? 'white' : 'lightgray'">current selection:</p>
+    <p>{{ membServ.selected | json }} </p>
+    <router-outlet></router-outlet>`,
+  styleUrls: ['app.component.css']
+})
+export class AppComponent{
+
+  constructor(private membServ: MemberService){}
+  
+}
