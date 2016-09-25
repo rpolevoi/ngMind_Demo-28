5c3eefb6d26f330bbcd8d7f91f12e11515a43c32
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-25 16:36:42 -0700

    Create route_a.component.ts

---
+++ app/route_a/route_a.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MemberService } from '../member.service';
+import { Member } from '../member';
+
+import { Router } from '@angular/router';
+
+
+@Component({
+  moduleId: module.id,
+  template: `<h1>Route A View Here</h1>
+              <app-child-component *ngFor = "let member of members | async" [childProp]="member"
+                (emitter)="select($event)">
+              </app-child-component>
+               <p [style.color]="membServ.selected ? 'black' : 'lightgray'">current selection: {{membServ.selected | json}}</p>`,
+  styleUrls: ['route_a.component.css']              
+  })
+export class RouteAComponent  {
+
+  members: Member[] = this.membServ.getMembers();
+
+  
+  select(memb: Member) {
+    this.router.navigate(['routeB', memb.id]);
+  }
+  
+  constructor(private membServ: MemberService, private router: Router){}
+  
+}
